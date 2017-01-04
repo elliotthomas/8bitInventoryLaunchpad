6c123cae98ffb8618c3fab8cd7b745b827375012
Elliot Thomas <elliotnthomas@gmail.com>
2017-01-03 21:35:33 -0600

    added search functionality

--- public/scripts/8bitInventory.js
+++ public/scripts/8bitInventory.js
@@ -1,50 +1,80 @@
-// properties by which searches can be done
-var sizes = [ 'small', 'medium', 'large' ];
-var colors = [ 'red', 'orange', 'yellow', 'green', 'mermaid treasure', 'blue', 'purple' ];
+var items = [];//global array of items in the inventory
 
-////// global array of items in inventory //////
-var items = [];
+$(document).ready(function(){
+  $('#addItemButton').on('click', function(){
+    addObject(event);
+    $('#addObject').val('');
+    $('#addColor').val('none');
+    $('#addSize').val('none');
+  });//end addItemButton on click function
 
-$( document ).ready( function(){
-  var addObject = function( colorIn, nameIn, sizeIn ){
-    console.log( 'in addObject' );
-    // assemble object from new fields
-    var newItem = {
-      color: colorIn,
-      name: nameIn,
-      size: sizeIn
-    }; // end testObject
-    console.log( 'adding:', newItem );
-    ////// TODO: add ajax call to addItem route to add this item to the table
-    // add to items array
-    items.push( newItem );
-  }; // end addObject
-
-  var findObject = function( colorCheck, sizeCheck ){
-    console.log( 'in findObject. Looking for:', colorCheck, sizeCheck );
-    // array of matches
-    var matches = [];
-    for ( var i = 0; i < items.length; i++ ) {
-      if( items[i].color == colorCheck && items[i].size == sizeCheck ){
-        // match, add to array
-        matches.push( items[i] );
-      } // end if
-    } // end for
-    console.log( 'matches:', matches );
-    ////// TODO: display matches
-  }; // end findObject
-
-  var getObjects = function(){
-    console.log( 'in getObjects');
-    // populate the items array
-    ////// TODO: replace the stuff in this function with getting items from the database ////////
-    ////// hint: make a get call to the getInventory and use it's response data to fill the items array ////////
-  }; // end getObjects
-
-  // get objects when doc is ready
-  getObjects();
-  // the below are tests to show what is returned when running findObject
-  addObject( 'blue', 'blueberry', 'small' )
-  findObject( 'blue', 'small' );
-  findObject( 'blue', 'large' );
+  $('#searchButton').on('click',function(){
+  //  console.log('searching...');
+    getObjects(event);
+  });//end searchButton onClick function
 }); // end doc ready
+
+var addObject = function(event){
+//  console.log( 'in addObject' );
+  // assemble object from new fields
+  event.preventDefault();
+  var newItem = {
+    name: $('#addObject').val(),
+    color: $('#addColor').val(),
+    size: $('#addSize').val()
+  }; // end newItem
+//  console.log( 'adding:', newItem );
+  $.ajax({
+    type: 'POST',
+    url: '/addItem',
+    data: newItem,
+    success: function(response){
+  //    console.log('back from POST call:', response);
+    },//end success function
+    error: function(){
+  //    console.log('error with addItem ajax call...');
+    }//end error function
+  });//end ajax call in addObject
+  //getObjects(event);
+}; // end addObject function
+
+var findObject = function(array){
+//  console.log( 'in findObject. Looking for:', $('#searchColor').val(), $('#searchSize').val());
+  // array of matches
+  var matches = [];
+  for ( var i = 0; i < items[0].length; i++ ) {
+    if(items[0][i].color == $('#searchColor').val() && items[0][i].size == $('#searchSize').val()){
+      // match, add to array
+      matches.push(items[0][i]);
+    } // end if
+  } // end for
+//  console.log( 'matches:', matches );
+  ////// TODO: display matches
+  displaySearchResults(matches);
+}; // end findObject function
+
+var displaySearchResults = function(array){
+  $('#outputDiv').html('');//clear outputDiv
+  if(array.length <1){
+    $('#outputDiv').append('<h3>' + 'There are no matches in the inventory' + '</h3>');
+  } else {
+    $('#outputDiv').append('<h4>Search Results:</h4>');
+    for(var i=0; i < array.length; i++ ){
+      $('#outputDiv').append('<p id="resultsDisplay">' + array[i].size + ' ' + array[i].color + ' ' + array[i].object_name + '</p>');
+    }//end for loop
+  }//end else statement
+};//end displaySearchResults function
+
+var getObjects = function(event){
+//  console.log( 'in getObjects');
+  event.preventDefault();
+  $.ajax({
+    type: 'GET',
+    url: '/getInventory',
+    success: function(response){
+  //    console.log('added to items array from get:', response);
+      items.push(response);
+      findObject(items);
+    }//end success function
+  });//end getObjects ajax
+}; // end getObjects function
--- server/app.js
+++ server/app.js
@@ -1,32 +1,65 @@
-var express = require( 'express' );
+var express = require('express');
 var app = express();
-var path = require( 'path' );
-var bodyParser = require( 'body-parser' );
-var urlEncodedParser = bodyParser.urlencoded( { extended: false } );
+var path = require('path');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+var urlEncodedParser = bodyParser.urlencoded({
+    extended: true
+});
 var port = process.env.PORT || 3003;
 
+//connection string to database named "inventory"
+//table named "items" wtih columns "object_name", "color", "size"
+var connectionString = 'postgres://localhost:5432/inventory';
+
+app.use(express.static('public')); //static folder
+
 // spin up server
-app.listen( port, function(){
-  console.log( 'server up on:', port );
+app.listen(port, function() {
+    console.log('server up on:', port);
 }); // end spin up server
 
 // base url
-app.get( '/', function( req, res ){
-  console.log( 'base url hit' );
-  res.sendFile( path.resolve( 'views/index.html' ) );
+app.get('/', function(req, res) {
+    console.log('base url hit');
+    res.sendFile(path.resolve('views/index.html'));
 }); // end home base
 
 // add new objects to the inventory
-app.post( '/addItem', urlEncodedParser, function( req, res ){
-  console.log( 'addItem route hit:', req.body );
-  // add the item from req.body to the table
+app.post('/addItem', urlEncodedParser, function(req, res) {
+    console.log('addItem route hit:', req.body);
+    // add the item from req.body to the table
+    pg.connect(connectionString, function(err, client, done) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('connected to database in post');
+            client.query('INSERT INTO items (object_name, color, size) values ($1, $2, $3)', [req.body.name, req.body.color, req.body.size]);
+            done();
+            res.send('go!');
+        }
+    }); //end connection to db
 }); // end addItem route
 
 // get all objects in the inventory
-app.get( '/getInventory', function( req, res ){
-  console.log( 'getInventory route hit' );
-  // get all items in the table and return them to client
-}); // end addItem route
-
-// static folder
-app.use( express.static( 'public' ) );
+app.get('/getInventory', function(req, res) {
+    console.log('getInventory route hit');
+    // get all items in the table and return them to client
+    pg.connect(connectionString, function(err, client, done) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('connected to db in getInventory');
+            var query = client.query('SELECT * from items');
+            var allItems = [];
+            query.on('row', function(row) {
+                allItems.push(row);
+            });
+            query.on('end', function() {
+                done();
+                console.log(allItems);
+                res.send(allItems);
+            });
+        }
+    }); //end pg connect in get
+}); //end getInventory route
